31adfa9888d1c2fddc59e2e719c7895a05c1b419
e.a.konovalova <e.a.konovalova@tinkoff.ru>
2021-01-05 23:25:56 +0300

    add random.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,34 +1,43 @@
 import readlineSync from 'readline-sync';
 import sayHello from '../cli.js';
+import getRandom from '../random.js';
 
 const name = sayHello();
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+const maxRounds = 3;
+const condition = (num) => ((num % 2 === 0) ? 'yes' : 'no');
+// const checkAnswer = (usersAnswer, correctAnswer) => {
+//   if (usersAnswer === correctAnswer) {
+//     console.log('Correct!');
+//   } else {
+//     console.log(`'${usersAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+//     console.log(`Let's try again, ${name}!`);
+//     break;
+//   }
+// };
 
 const even = () => {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  console.log(description);
 
-  for (let i = 0; i < 3; i += 1) {
-    // create num
-    const num = Math.floor(Math.random() * 100);
-    // check truth
-    let truth;
-    if (num % 2 === 0) {
-      truth = 'yes';
-    } else {
-      truth = 'no';
-    }
+  for (let i = 0; i < maxRounds; i += 1) {
+
+    const num = getRandom();
+
+    const truth = condition(num);
 
     // get answer
     const answer = readlineSync.question(`Question: ${num} `);
-    // check answer
+
     if (answer === truth) {
       console.log('Correct!');
-      if (i === 2) {
-        console.log(`Congratulations, ${name}!`);
-      }
     } else {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${truth}'.`);
       console.log(`Let's try again, ${name}!`);
       break;
+    }
+
+    if (i === maxRounds - 1) {
+      console.log(`Congratulations, ${name}!`);
     }
   }
 };
