81c8d01c3867f314719e586d55231aa40279f472
e.a.konovalova <e.a.konovalova@tinkoff.ru>
2021-01-05 13:49:10 +0300

    add brain-even.js

---
+++ bin/brain-even.js
@@ -0,0 +1,29 @@
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}!`);
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+for (let i = 0; i < 3; i += 1) {
+  // create num
+  const num = Math.floor(Math.random() * 100);
+  // check truth
+  let truth;
+  if (num % 2 === 0) {
+    truth = 'yes';
+  } else {
+    truth = 'no';
+  }
+
+  // get answer
+  const answer = readlineSync.question(`Question: ${num} `);
+  // check answer
+  if (answer === truth) {
+    console.log('Correct!');
+  } else {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${truth}'.`);
+    console.log(`Let's try again, ${name}!`);
+    break;
+  }
+}
