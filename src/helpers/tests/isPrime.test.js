23b174e9e53ebb8690eaed11c4324f75dfd8097e
e.a.konovalova <e.a.konovalova@tinkoff.ru>
2021-01-08 20:42:05 +0300

    add tests for prime.js

---
+++ src/helpers/tests/isPrime.test.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+import { strict as assert } from 'assert';
+import prime from '../isPrime.js';
+
+assert(prime(0) === 'no');
+assert(prime(1) === 'no');
+assert(prime(2) === 'yes');
+assert(prime(3) === 'yes');
+assert(prime(4) === 'no');
+assert(prime(7) === 'yes');
+
+assert(prime(24) === 'no');
+assert(prime(13) === 'yes');
+
+// chmod +x src/helpers/tests/isPrime.test.js
